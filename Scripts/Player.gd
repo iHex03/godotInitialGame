1f8c1ad521d0069f22257f5761ed1780763c3882
Andre I Pedroso <andreiguezli@gmail.com>
2023-10-20 14:30:59 -0300

    começo

---
+++ Scripts/Player.gd
@@ -0,0 +1,58 @@
+extends CharacterBody3D
+
+@onready var camera_pivot = $CameraPivot
+
+var sens_horizontal = 0.5
+var sens_vertical = 0.3
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+var is_locked = true
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event):
+	if is_locked:
+		if event is InputEventMouseMotion:
+			rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
+			camera_pivot.rotate_x(deg_to_rad(-event.relative.y * sens_vertical))
+			#visual.rotate_x(deg_to_rad(-event.relative.y * sens_vertical))
+			
+			camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, deg_to_rad(-60), deg_to_rad(60))
+			#visual.rotation.x = clamp(visual.rotation.x, deg_to_rad(-60), deg_to_rad(60))
+
+func _physics_process(delta):
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			is_locked = true
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			is_locked = false
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
